5cd0dfa442c131d4822cfc17fa09f99e75ea6415
Nachiket-Patel <new.computer.sci.tech@gmail.com>
2022-10-02 23:43:51 +0530

    start level up 1 to 2

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    int n,s;
+    while(t--){
+        cin >> n >> s;
+        cout << s-n << endl;
+    }
+    return 0;
+}
