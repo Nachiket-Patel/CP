3183fb2dd9277f447d265bf551bd84dc8720ec59
Nachiket-Patel <new.computer.sci.tech@gmail.com>
2022-10-21 10:30:38 +0530

    day 17 codechef

---
+++ CodeChef/Contests/Starter 60 (div - 4)/Card_Removal.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n,a[n];
+        cin >> n;
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+        }
+        for(int j = 0; j < n; j++){
+            cout << a[j];
+        }
+        cout << endl;
+        for(int k = n; k > 1; k++){
+            for(int l = 0; l < n-1; l++)
+        }
+
+    }
+    return 0;
+}
