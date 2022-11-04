622a72e046dc6b9dc71b9d7a096b5e9e2b0c32e9
Nachiket-Patel <new.computer.sci.tech@gmail.com>
2022-11-04 23:26:28 +0530

    add file 12

---
+++ CodeChef/level up 1 to 2/Basic programming concepts/12-Turn_It.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int u,v,a,s;
+        cin >> u >> v >> a >> s;
+        if((u*u)-(2*a*s) <= (v*v)){
+            cout << "Yes\n";
+        }
+        else{
+            cout << "No\n";
+        }
+    }
+    return 0;
+}
