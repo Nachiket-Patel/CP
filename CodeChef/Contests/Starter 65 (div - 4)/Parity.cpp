154cccc3edede5b24f4af15f28d821257b91953d
Nachiket-Patel <new.computer.sci.tech@gmail.com>
2022-11-16 21:39:58 +0530

    first problem

---
+++ CodeChef/Contests/Starter 65 (div - 4)/Parity.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        if(n%2==0){
+            cout << "Yes\n";
+        }
+        else{
+            cout << "No\n";
+        }
+    }
+    return 0;
+}
