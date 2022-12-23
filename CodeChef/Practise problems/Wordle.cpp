2521cf20ced8f713e630acb9b75883cf99cfddad
Nachiket-Patel <new.computer.sci.tech@gmail.com>
2022-12-23 22:02:48 +0530

    add file 3

---
+++ CodeChef/Practise problems/Wordle.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        string s,t,m;
+        cin >> s >> t;
+        for(int i=0; i<5; i++){
+            if(s[i]==t[i]){
+                cout << "G";
+            }
+            else{
+                cout << "B";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
