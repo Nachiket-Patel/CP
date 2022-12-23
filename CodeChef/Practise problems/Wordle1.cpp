e47709cf5273d004b02b11275adefe66bbee66f5
Nachiket-Patel <new.computer.sci.tech@gmail.com>
2022-12-23 22:12:42 +0530

    file 3 my solution

---
+++ CodeChef/Practise problems/Wordle1.cpp
@@ -0,0 +1,25 @@
+//This is my solution 
+
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    string s,t,M;	   
+	    cin>>s>>t;
+	    M="*****";
+	    for(int i =0; i<5; i++){
+	        if(s[i]==t[i]){
+	            M[i]='G';
+	        }
+	        else{
+	           M[i]='B';
+	        }
+	        cout<<M<<endl;
+	    }
+	    return 0;
+    }
+}
